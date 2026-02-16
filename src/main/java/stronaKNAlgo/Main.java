0b34f5980c35aa441e92e57a0c4061c5799ed5e9
Adam Kruszczyński <290446@student.pwr.edu.pl>
2026-02-16 22:15:54 +0100

    Add backend base with working register and login.

---
+++ src/main/java/stronaKNAlgo/Main.java
@@ -0,0 +1,11 @@
+package stronaKNAlgo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Main {
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+}
