8e5c54d1aec69f642b8f5d91f37d115783dd6010
Mateusz <mateuszkozik2804@gmail.com>
2026-02-26 21:31:39 +0100

    fix:Fix some pre-commit erros and added comments to code

--- backend/src/main/java/algo/module/PostType.java
+++ backend/src/main/java/algo/module/PostType.java
@@ -1,8 +1,13 @@
 package algo.module;
 
-public enum PostType{
-    STANDARD,
-    NEWS,
-    TEMP_STANDARD,
-    TEMP_NEWS
+/** Defines available post types used in the system. */
+public enum PostType {
+  /** Standard post type. */
+  STANDARD,
+  /** News post type. */
+  NEWS,
+  /** Temporary standard post type. */
+  TEMP_STANDARD,
+  /** Temporary news post type. */
+  TEMP_NEWS
 }
