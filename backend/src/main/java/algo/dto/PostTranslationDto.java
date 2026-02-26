d4f034234689d935f53ac3fe4641cd21cefb94dc
Mateusz <mateuszkozik2804@gmail.com>
2026-02-26 21:29:17 +0100

    fix:Fix some pre-commit erros and added comments to code

--- backend/src/main/java/algo/dto/PostTranslationDto.java
+++ backend/src/main/java/algo/dto/PostTranslationDto.java
@@ -1,14 +1,28 @@
 package algo.dto;
 
-
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
-
+/**
+ * @param postId id of translated record
+ * @param languageCode language translated code
+ * @param title translated title
+ * @param shortDescription short translated description
+ * @param fullDescription full translated description
+ */
 public record PostTranslationDto(
-        Long id,
-        @NotBlank @Size(max = 5) String languageCode,
-        @NotBlank @Size(max = 200) String title,
-        @NotBlank @Size(max = 400) String shortDescription,
-        @NotBlank String fullDescription
-) { }
+    Long postId,
+    @NotBlank @Size(max = LANG_CODE_LENGTH) String languageCode,
+    @NotBlank @Size(max = TITLE_LENGTH) String title,
+    @NotBlank @Size(max = SHORT_DESC_LENGTH) String shortDescription,
+    @NotBlank String fullDescription) {
+
+  /** Max length of language code column. */
+  public static final int LANG_CODE_LENGTH = 5;
+
+  /** Max length of title column. */
+  public static final int TITLE_LENGTH = 200;
+
+  /** Max length of short description column. */
+  public static final int SHORT_DESC_LENGTH = 400;
+}
