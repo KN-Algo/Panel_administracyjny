e7298bb55077e2496f4427760dd9b826f6db8e40
Mateusz <mateuszkozik2804@gmail.com>
2026-03-04 00:08:23 +0100

    feat(announcement_module): add TempAnnouncementModule entity for homepage announcements

---
+++ backend/src/main/java/algo/module/TempAnnouncementModule.java
@@ -0,0 +1,50 @@
+package algo.module;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/** DB entity for temporary homepage announcements. */
+@Entity
+@Table(name = "announcements")
+@Data
+@NoArgsConstructor
+public class TempAnnouncementModule {
+
+  /**
+   * @param announcementId id of announcement
+   */
+  @Id
+  @GeneratedValue(strategy = IDENTITY)
+  private Long announcementId;
+
+  /**
+   * @param linkUrl full link URL
+   */
+  @Column(name = "link_url", nullable = false)
+  private String linkUrl;
+
+  /**
+   * @param imageUrl full iamge URL
+   */
+  @Column(name = "image_url", nullable = false)
+  private String imageUrl;
+
+  /**
+   * @param startsDate the day that announcement starts
+   */
+  @Column(name = "starts_date", nullable = false)
+  private Long startsDate;
+
+  /**
+   * @param expiressDate the day that announcement ends
+   */
+  @Column(name = "expires_date", nullable = false)
+  private Long expiresDate;
+}
