bc566663710fd8946df55c25463d97fdb60134b0
Mateusz Kozik <mateuszkozik2804@gmail.com>
2026-03-04 14:23:45 +0100

    frat(project_type): Added enum to recognize type of Project

--- backend/src/main/java/algo/module/ProjectType.java
+++ backend/src/main/java/algo/module/ProjectType.java
@@ -1,4 +1,14 @@
 package algo.module;
-
-public class ProjectType {
+/**
+ * Defines avaiable project type
+ */
+public enum ProjectType {
+    /**
+     * FINISHED project type
+     */
+    FINISHED,
+    /**
+     * INCOMMING project type
+     */
+    INCOMMING
 }
