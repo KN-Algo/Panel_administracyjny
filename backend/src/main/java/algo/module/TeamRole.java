adecfd5790892d950f05cd56e178de0055e4cd97
Adam Kruszczyński <290446@student.pwr.edu.pl>
2026-09-07 17:18:32 +0200

    feat: create TeamRole enum

---
+++ backend/src/main/java/algo/module/TeamRole.java
@@ -0,0 +1,11 @@
+package algo.module;
+
+/**
+ * Defines the logical group of a person for sorting and displaying sections in the "Team" tab.
+ */
+public enum TeamRole {
+    BOARD,
+    MEMBER,
+    SUPERVISOR,
+    MASCOT
+}
